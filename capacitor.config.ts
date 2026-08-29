37b18bb9f4ba441c317376ceebf34bc72c37d9ec
Borna17 <69678243+Borna17@users.noreply.github.com>
2026-08-29 13:42:15 +0200

    Align Capacitor app ID with Google Play package

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'hr.fersys.app',
+  appId: 'com.fersys.app',
   appName: 'FERSYS',
   webDir: 'dist',
   server: {
