62d3d7289e357e70d1413b9385e414c99feb61de
Borna17 <69678243+Borna17@users.noreply.github.com>
2026-09-09 21:37:41 +0200

    Rename native app display name to FERSYS Business

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'com.fersys.app',
-  appName: 'FERSYS',
+  appName: 'FERSYS Business',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
