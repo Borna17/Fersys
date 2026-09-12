c544de0d70bf71284c7930e7b703a99ba28473e1
Borna17 <69678243+Borna17@users.noreply.github.com>
2026-09-12 20:29:49 +0200

    Stabilize core app startup across web and native

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-import CroatianNavigationLabels from './components/CroatianNavigationLabels'
-import UniversalDraftProtection from './components/UniversalDraftProtection'
-import {
-  IncomingInvoicesCloudBridge,
-} from './components/IncomingInvoicesCloudBridge'
-import WebPushForegroundListener from './components/WebPushForegroundListener'
-import WorkOrderEditQuantityTextFix from './components/WorkOrderEditQuantityTextFix'
-import {
-  WorkOrderShareAction,
-} from './components/WorkOrderShareAction'
 import DeleteAccountPage from './pages/DeleteAccountPage'
 import { AppRouter } from './router/AppRouter'
 
@@ -16,17 +6,7 @@ function App() {
     return <DeleteAccountPage />
   }
 
-  return (
-    <>
-      <CroatianNavigationLabels />
-      <UniversalDraftProtection />
-      <IncomingInvoicesCloudBridge />
-      <WebPushForegroundListener />
-      <WorkOrderEditQuantityTextFix />
-      <AppRouter />
-      <WorkOrderShareAction />
-    </>
-  )
+  return <AppRouter />
 }
 
 export default App
